38f63a4f5ffb90d5409927149d0cb3ea64655bfc
Davide Chiocca <sharpeyedvision1@gmail.com>
2025-07-18 16:31:00 +0200

    Aggiunta bottone "modifica" attività

--- src/main/java/com/unina/biogarden/gui/controller/home/widget/ActivityCardController.java
+++ src/main/java/com/unina/biogarden/gui/controller/home/widget/ActivityCardController.java
@@ -4,6 +4,7 @@ import com.unina.biogarden.model.Attivita;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.Button;
 
 public class ActivityCardController {
     @FXML private Label nomeProgettoLabel;
@@ -13,6 +14,9 @@ public class ActivityCardController {
     @FXML private Label dataFineLabel;
     @FXML private Label tipoLabel;
     @FXML private Label statoLabel;
+    @FXML private Button modificaAttivitaLink;
+
+    private Attivita attivitaCorrente;
 
     public void setData(Attivita attivita) {
         nomeProgettoLabel.setText(attivita.getNomeProgetto());
@@ -22,5 +26,14 @@ public class ActivityCardController {
         dataFineLabel.setText(attivita.getDataFine().toString());
         tipoLabel.setText(attivita.getTipo());
         statoLabel.setText(attivita.getStato());
+
+        this.attivitaCorrente = attivita;
     }
+
+    @FXML private void handleModificaAttivita() {
+        modificaAttivitaLink.getScene().getWindow().setUserData(attivitaCorrente); // Per semplice passaggio di dati tra schermate
+        Attivita a = (Attivita) modificaAttivitaLink.getScene().getWindow().getUserData();
+        System.out.println(a);
+    }
+
 }
